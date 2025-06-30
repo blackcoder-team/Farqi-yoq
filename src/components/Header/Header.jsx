7a568bcca5d448bf7b3b3b95d455705c180a3182
qoramen <tojiboyevmurodbbek@gmail.com>
2025-06-30 12:45:11 +0500

    header, about, musics sections

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,32 +3,40 @@ import './Header.css'
 import '../app/App.css'
 
 const Header = () => {
-  return (
-    <header>
-        <nav>
-            <div className="container">
-                <div className="row justify-content-between align-center">
-                    <div className="col-3 align-center">
-                        <h1 className='m-0'>FARQI YO'Q</h1>
+    return (
+        <header>
+            <nav>
+                <div className="container">
+                    <div className="row justify-content-between align-center">
+                        <div className="col-3 align-center">
+                            <h1 className='m-0'>FARQI YO'Q</h1>
+                        </div>
+                        <div className="col-8 d-flex align-center justify-content-end">
+                            <ul>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Musics</a></li>
+                                <li><a href="">Quotes</a></li>
+                                <li><a href="">Movies</a></li>
+                                <li><a href="">Share With Us</a></li>
+                                <li><a href="">Twits</a></li>
+                                <li><a href="">Contact Us</a></li>
+                                <li className='sign-in'><a href="">Sign In</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="col-8 d-flex align-center justify-content-end">
-                        <ul>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">About</a></li>
-                            <li><a href="">Musics</a></li>
-                            <li><a href="">Quotes</a></li>
-                            <li><a href="">Movies</a></li>
-                            <li><a href="">Share With Us</a></li>
-                            <li><a href="">Twits</a></li>
-                            <li><a href="">Contact Us</a></li>
-                            <li className='sign-in'><a href="">Sign In</a></li>
-                        </ul>
+                </div>
+            </nav>
+            <div className="hero">
+                <div className="container">
+                    <div className="text">
+                        <h1>Farqi yo'q</h1>
+                        <p>Qanaqa qilsang ham farqi yo'q, lekin qil!</p>
                     </div>
                 </div>
             </div>
-        </nav>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '../Header/Header'
+import About from "../About/About";
+import Musics from "../Musics/Musics";
 
 function App() {
 
   return (
     <>
       <Header/>
+      <main>
+        <About/>
+        <Musics/>
+      </main>
     </>
   )
 }
